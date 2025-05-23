ea09d852b9eaa67d24016277943bc621c424b1be
kiwoonjung <kiwoon0627@gmail.com>
2025-05-23 13:43:57 -0700

    storeAuthStateInCookie value changed at authConfig.ts

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -13,7 +13,7 @@ export const msalConfig: Configuration = {
   },
   cache: {
     cacheLocation: 'sessionStorage',
-    storeAuthStateInCookie: false,
+    storeAuthStateInCookie: true,
   },
   system: {
     loggerOptions: {
